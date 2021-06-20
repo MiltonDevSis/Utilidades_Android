f33d425bcd8ac4953488c59e375cf1c29c9b5daa
MiltonDevSis <milton.dev.sis@gmail.com>
2021-06-20 18:48:04 -0300

    Projeto concluido com o metodo startActivityForResult depreciado

--- sendDataStartForResult/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ sendDataStartForResult/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var textResult: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val edtPrimeiro = findViewById<EditText>(R.id.edtNumero1)
+        val edtSegundo = findViewById<EditText>(R.id.edtNumero2)
+        val buttonFirst = findViewById<Button>(R.id.btnOne)
+        textResult = findViewById(R.id.txtResultado)
+
+        buttonFirst.setOnClickListener {
+
+            val numeroUm = edtPrimeiro.text.toString()
+            val numeroDois = edtSegundo.text.toString()
+
+            if (numeroUm == "" || numeroDois == ""){
+                Toast.makeText(applicationContext, "Os campos são obrigatórios!", Toast.LENGTH_LONG).show()
+            }else{
+                val numeroUm = Integer.parseInt(edtPrimeiro.text.toString())
+                val numeroDois = Integer.parseInt(edtSegundo.text.toString())
+
+                val intent = Intent(this@MainActivity, SecondActivity::class.java)
+                intent.putExtra("numero1", numeroUm)
+                intent.putExtra("numero2", numeroDois)
+                startActivityForResult(intent, 1)
+            }
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 1){
+            if (resultCode == RESULT_OK){
+                val result = data?.getIntExtra("resultado", 0)
+                textResult!!.text = " $result"
+            }
+
+            if (resultCode == RESULT_CANCELED){
+                textResult!!.text = getString(R.string.nada_selecionado)
+            }
+        }
     }
 }
--- sendDataStartForResult/app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ sendDataStartForResult/app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -1,11 +1,46 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        title = "Activity 2"
+
+        val intent = intent
+        val numero1 = intent.getIntExtra("numero1", 0)
+        val numero2 = intent.getIntExtra("numero2", 0)
+
+        val textView = findViewById<TextView>(R.id.txtSegundaActivity)
+        textView.text = "Os Números são $numero1 , $numero2"
+
+        val buttonAdd = findViewById<Button>(R.id.btnAdd)
+        val buttonSubtract = findViewById<Button>(R.id.btnSubtract)
+
+        buttonAdd.setOnClickListener {
+            val resultado = numero1 + numero2
+            val resultIntent = Intent()
+            resultIntent.putExtra("resultado", resultado)
+
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
+
+        buttonSubtract.setOnClickListener {
+            val resultado = numero1 - numero2
+            val resultIntent = Intent()
+            resultIntent.putExtra("resultado", resultado)
+
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
     }
 }
