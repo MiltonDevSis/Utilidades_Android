1b254b1f7dce898f1c53e0e9bc4679058ab66e82
MiltonDevSis <milton.dev.sis@gmail.com>
2021-06-26 17:17:17 -0300

    projeto retrofit com ApiRest criado

---
+++ ApiRest_Retrofit/app/src/main/java/com/example/retrofit_apirest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.retrofit_apirest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
