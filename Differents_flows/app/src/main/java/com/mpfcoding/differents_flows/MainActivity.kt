e88154353c9350e09b077b5280e657d97781f464
MiltonDevSis <milton.dev.sis@gmail.com>
2021-10-30 14:10:01 -0300

    Projeto para estudar os diferentes tipos de flows

---
+++ Differents_flows/app/src/main/java/com/mpfcoding/differents_flows/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mpfcoding.differents_flows
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
