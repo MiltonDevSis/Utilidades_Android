94c6f1d90ea3d314efc46dbbd6630b4d4f3047e7
Milton <milton.dev.sis@gmail.com>
2022-01-02 20:45:34 -0300

    Project New Api Splash screen working

--- New_Api_SplashScreen/app/src/main/java/com/mpfcoding/new_api_splash_screen/MainActivity.kt
+++ New_Api_SplashScreen/app/src/main/java/com/mpfcoding/new_api_splash_screen/MainActivity.kt
@@ -3,36 +3,39 @@ package com.mpfcoding.new_api_splash_screen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.mpfcoding.new_api_splash_screen.ui.theme.New_Api_Splash_ScreenTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepVisibleCondition{
+                viewModel.isLoading.value
+            }
+        }
         setContent {
             New_Api_Splash_ScreenTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ){
+                    Text(text = "Hello World!")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    New_Api_Splash_ScreenTheme {
-        Greeting("Android")
     }
 }
