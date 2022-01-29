a3394cc0422de576110a1ab0d0d2c30ebc557927
Milton <milton.dev.sis@gmail.com>
2022-01-29 17:25:45 -0300

    Adicionado função para transformar para base 64

--- Capture_Image_Camera/app/src/main/java/com/mpfcoding/capture_image_camera/MainActivity.kt
+++ Capture_Image_Camera/app/src/main/java/com/mpfcoding/capture_image_camera/MainActivity.kt
@@ -6,15 +6,19 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.mpfcoding.capture_image_camera.util.ImageUtil
 
 class MainActivity : AppCompatActivity() {
 
     private var image1: ImageView? = null
     private var image2: ImageView? = null
+    private var imageButton1: ImageView? = null
+    private var imageButton2: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,17 +26,19 @@ class MainActivity : AppCompatActivity() {
 
         image1 = findViewById(R.id.capture_image_1)
         image2 = findViewById(R.id.capture_image_2)
+        imageButton1 = findViewById(R.id.button_image_01)
+        imageButton2 = findViewById(R.id.button_image_02)
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 100)
         }
 
-        image1!!.setOnClickListener {
+        imageButton1!!.setOnClickListener {
             val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             startActivityForResult(intent, 100)
         }
 
-        image2!!.setOnClickListener {
+        imageButton2!!.setOnClickListener {
             val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             startActivityForResult(intent, 101)
         }
@@ -41,13 +47,19 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 100){
-            val captureImage = data!!.extras!!.get("data") as Bitmap
+            val captureImage1 = data!!.extras!!.get("data") as Bitmap
 
-            image1!!.setImageBitmap(captureImage)
+            val teste = ImageUtil.bitmapToBase64(captureImage1)
+
+            image1!!.setImageBitmap(captureImage1)
+            imageButton1!!.visibility = View.GONE
         }else if (requestCode == 101){
-            val captureImage = data!!.extras!!.get("data") as Bitmap
+            val captureImage2 = data!!.extras!!.get("data") as Bitmap
 
-            image2!!.setImageBitmap(captureImage)
+            val teste = ImageUtil.bitmapToBase64(captureImage2)
+
+            image2!!.setImageBitmap(captureImage2)
+            imageButton2!!.visibility = View.GONE
         }
     }
 }
---
+++ Capture_Image_Camera/app/src/main/java/com/mpfcoding/capture_image_camera/util/ImageUtil.kt
@@ -0,0 +1,25 @@
+package com.mpfcoding.capture_image_camera.util
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+import java.io.ByteArrayOutputStream;
+
+class ImageUtil {
+
+    companion object{
+        fun base64ToBitmap(base64Str: String): Bitmap? {
+            val decodedBytes: ByteArray = Base64.decode(
+                base64Str.substring(base64Str.indexOf(",") + 1),
+                Base64.DEFAULT
+            )
+            return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size)
+        }
+
+        fun bitmapToBase64(bitmap: Bitmap): String? {
+            val outputStream = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+            return Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT)
+        }
+    }
+}
