bfa780dc9c278ea193ac1bcd17717b3b94b82687
Milton <milton.dev.sis@gmail.com>
2022-01-29 17:08:38 -0300

    Capturando imagens e setando na tela

--- Capture_Image_Camera/app/src/main/java/com/mpfcoding/capture_image_camera/MainActivity.kt
+++ Capture_Image_Camera/app/src/main/java/com/mpfcoding/capture_image_camera/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.mpfcoding.capture_image_camera
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var image1: ImageView? = null
+    private var image2: ImageView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        image1 = findViewById(R.id.capture_image_1)
+        image2 = findViewById(R.id.capture_image_2)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 100)
+        }
+
+        image1!!.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, 100)
+        }
+
+        image2!!.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, 101)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 100){
+            val captureImage = data!!.extras!!.get("data") as Bitmap
+
+            image1!!.setImageBitmap(captureImage)
+        }else if (requestCode == 101){
+            val captureImage = data!!.extras!!.get("data") as Bitmap
+
+            image2!!.setImageBitmap(captureImage)
+        }
     }
 }
