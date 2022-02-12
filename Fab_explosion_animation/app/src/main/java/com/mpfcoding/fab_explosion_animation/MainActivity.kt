b1d6ddd821fdb574728c1083e1b0c3cb1c2d0ae7
Milton <milton.dev.sis@gmail.com>
2022-02-12 17:25:51 -0300

    Finish fab explosion

--- Fab_explosion_animation/app/src/main/java/com/mpfcoding/fab_explosion_animation/MainActivity.kt
+++ Fab_explosion_animation/app/src/main/java/com/mpfcoding/fab_explosion_animation/MainActivity.kt
@@ -2,10 +2,39 @@ package com.mpfcoding.fab_explosion_animation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
+import com.mpfcoding.fab_explosion_animation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val animation = AnimationUtils.loadAnimation(this, R.anim.circle_explosion_anim).apply {
+            duration = 1000
+            interpolator = AccelerateDecelerateInterpolator()
+        }
+
+        binding.fab.setOnClickListener {
+            binding.fab.isVisible = false
+            binding.circle.isVisible = true
+            binding.circle.startAnimation(animation) {
+                // display your fragment
+                binding.root.setBackgroundColor(ContextCompat.getColor(this, R.color.purple_500))
+                binding.circle.isVisible = false
+
+                // open next screen
+
+            }
+        }
     }
 }
