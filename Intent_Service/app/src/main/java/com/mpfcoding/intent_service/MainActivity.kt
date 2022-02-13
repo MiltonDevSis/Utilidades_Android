74b588c27dd7b4dc4dca2cde065135119955542e
Milton <milton.dev.sis@gmail.com>
2022-02-13 14:28:54 -0300

    Service running

--- Intent_Service/app/src/main/java/com/mpfcoding/intent_service/MainActivity.kt
+++ Intent_Service/app/src/main/java/com/mpfcoding/intent_service/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.mpfcoding.intent_service
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mpfcoding.intent_service.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonStart.setOnClickListener {
+            Intent(this, MyIntentService::class.java).also {
+                startService(it)
+                binding.textService.text = "Service running"
+            }
+        }
+
+        binding.buttonStop.setOnClickListener {
+            MyIntentService.stopService()
+            binding.textService.text = "Service stopped"
+        }
     }
 }
