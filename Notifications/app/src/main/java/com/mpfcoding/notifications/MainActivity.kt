9d0efdc14c1c3c75059d202551a5992d8ac2aa49
Milton <milton.dev.sis@gmail.com>
2022-02-13 12:56:28 -0300

    Event click in notification implemented

--- Notifications/app/src/main/java/com/mpfcoding/notifications/MainActivity.kt
+++ Notifications/app/src/main/java/com/mpfcoding/notifications/MainActivity.kt
@@ -2,22 +2,22 @@ package com.mpfcoding.notifications
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import com.mpfcoding.notifications.constants.Notification
 import com.mpfcoding.notifications.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
-    val CHANNEL_ID = "channelID"
-    val CHANNEL_NAME = "channelName"
-    val NOTIFICATION_ID = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,25 +25,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         createNotificationChannel()
 
-        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+        val intent = Intent(this, MainActivity::class.java)
+
+        val pendingIntent = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(intent)
+            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+        val notification = NotificationCompat.Builder(this, Notification.CHANNEL_ID)
             .setContentTitle("Awesome notification!")
             .setContentText("This is the content Text")
             .setSmallIcon(R.drawable.ic_star)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
             .build()
 
         val notificationManager = NotificationManagerCompat.from(this)
 
         binding.buttonNotification.setOnClickListener {
-            notificationManager.notify(NOTIFICATION_ID, notification)
+            notificationManager.notify(Notification.NOTIFICATION_ID, notification)
         }
     }
 
-    fun createNotificationChannel(){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME,
-                NotificationManager.IMPORTANCE_DEFAULT).apply {
-                  lightColor = Color.GREEN
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                Notification.CHANNEL_ID, Notification.CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                lightColor = Color.GREEN
                 enableLights(true)
             }
             val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
---
+++ Notifications/app/src/main/java/com/mpfcoding/notifications/constants/Constants.kt
@@ -0,0 +1,7 @@
+package com.mpfcoding.notifications.constants
+
+object Notification {
+    const val CHANNEL_ID = "channelID"
+    const val CHANNEL_NAME = "channelName"
+    const val NOTIFICATION_ID = 0
+}
