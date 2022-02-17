3a7392f7e52572d87151977bbb969860e74e7467
Milton <milton.dev.sis@gmail.com>
2022-02-16 21:31:04 -0300

    Project to learn broadcast receiver finished

--- Broadcast_Receiver/app/src/main/java/com/mpfcoding/broadcast_receiver/MainActivity.kt
+++ Broadcast_Receiver/app/src/main/java/com/mpfcoding/broadcast_receiver/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.mpfcoding.broadcast_receiver
 
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var receiver: AirPlaneModeChangedReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        receiver = AirPlaneModeChangedReceiver()
+
+        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
+            registerReceiver(receiver, it)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(receiver)
     }
 }
