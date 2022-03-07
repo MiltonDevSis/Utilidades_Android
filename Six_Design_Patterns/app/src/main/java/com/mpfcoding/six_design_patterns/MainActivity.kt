ba3a32cdf797b8bec875c908d6be30abf11db1be
Milton <milton.dev.sis@gmail.com>
2022-03-07 17:07:17 -0300

    Isolated code for the singleton pattern

--- Six_Design_Patterns/app/src/main/java/com/mpfcoding/six_design_patterns/MainActivity.kt
+++ Six_Design_Patterns/app/src/main/java/com/mpfcoding/six_design_patterns/MainActivity.kt
@@ -4,16 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val message = "Hello word"
+        singletonPattern()
+    }
 
+    private fun singletonPattern(){
+        val message = "Hello word"
         val messageTwo = Singleton.message
 
         Singleton.showMessage(message)
-
         Singleton.showMessageTwo(messageTwo)
     }
 }
