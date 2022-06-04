e2d568cbae51fbfe2717f43267ceee2c9e6c1b3a
Milton <milton.dev.sis@gmail.com>
2022-06-04 17:30:04 -0300

    Teste live data e placeholders

--- TestHilt/app/src/main/java/com/mpfcoding/testhilt/MainActivity.kt
+++ TestHilt/app/src/main/java/com/mpfcoding/testhilt/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.mpfcoding.testhilt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
-import com.bumptech.glide.Glide
+import android.widget.*
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import com.mpfcoding.testhilt.glide.GlideImageLoaderImpl
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -16,10 +21,16 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var testeString: String
 
+    private val viewModel: MainViewModel by viewModels()
+
     private lateinit var buttonTest: Button
+    private lateinit var editUm: EditText
+    private lateinit var editDois: EditText
+    private lateinit var buttonSoma: Button
+    private lateinit var buttonMultiplica: Button
+    private lateinit var txtResultado: TextView
 
     private lateinit var imageGlide: ImageView
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,11 +38,17 @@ class MainActivity : AppCompatActivity() {
 
         mapComponents()
         setListeners()
+        observers()
     }
 
     private fun mapComponents() {
         buttonTest = findViewById(R.id.buttonTest)
         imageGlide = findViewById(R.id.imageFromGlide)
+        editUm = findViewById(R.id.editNumeroUm)
+        editDois = findViewById(R.id.editNumeroDois)
+        buttonSoma = findViewById(R.id.buttonSoma)
+        buttonMultiplica = findViewById(R.id.buttonMultiplica)
+        txtResultado = findViewById(R.id.textResultado)
     }
 
     private fun setListeners() {
@@ -41,5 +58,32 @@ class MainActivity : AppCompatActivity() {
 
         GlideImageLoaderImpl()
             .load(imageGlide, "https://miro.medium.com/max/1400/1*o8Q_O-C6yGZQqW_2cdafoQ.png")
+
+        buttonSoma.setOnClickListener {
+            sendDataToViewModel(1)
+        }
+
+        buttonMultiplica.setOnClickListener {
+            sendDataToViewModel(2)
+        }
+    }
+
+    private fun sendDataToViewModel(tipo: Int) {
+        val numeroUm = editUm.text.toString()
+        val numeroDois = editDois.text.toString()
+        if (numeroUm.isNotEmpty() && numeroDois.isNotEmpty()) {
+            viewModel.operation(numeroUm.toInt(), numeroDois.toInt(), tipo)
+        }
+    }
+
+    private fun observers() {
+        viewModel.liveData.observe(this) { result ->
+            txtResultado.text = getString(
+                R.string.o_resultado_e, result.toString(),
+                (result + 2).toString(),
+                (result + 3).toString(),
+                (result + 4).toString()
+            )
+        }
     }
 }
--- TestHilt/app/src/main/java/com/mpfcoding/testhilt/MainViewModel.kt
+++ TestHilt/app/src/main/java/com/mpfcoding/testhilt/MainViewModel.kt
@@ -1,10 +1,24 @@
 package com.mpfcoding.testhilt
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(): ViewModel() {
+class MainViewModel @Inject constructor() : ViewModel() {
 
+    private val _liveData = MutableLiveData(0)
+    var liveData: LiveData<Int> = _liveData
+
+    fun operation(um: Int, dois: Int, tipo: Int) {
+        _liveData.value = if(tipo == 1){
+            um + dois
+        } else{
+            um * dois
+        }
+    }
 }
